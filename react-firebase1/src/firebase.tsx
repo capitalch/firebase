52babf40440f60d2288ec8c0f01114276ce917a8
SushantAgrawal <sagarwal@netwoven.com>
2019-02-14 13:12:19 +0530

    init

---
+++ react-firebase1/src/firebase.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import firebase from 'firebase';
+var config = {
+    apiKey: "AIzaSyBpyJ3U6yLo3UgbkP1szU0ngg1wTSPr5ng",
+    authDomain: "adam1-9d728.firebaseapp.com",
+    databaseURL: "https://adam1-9d728.firebaseio.com",
+    projectId: "adam1-9d728",
+    storageBucket: "adam1-9d728.appspot.com",
+    messagingSenderId: "288882041176"
+};
+firebase.initializeApp(config);
+export default firebase;
